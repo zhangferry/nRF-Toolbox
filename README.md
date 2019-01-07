4088b6cc360b7391f7e9123269fe39d51495d289
zhangferry <zhangfei@dailyinnovation.biz>
2019-01-07 17:34:07 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# 提示
+
+感谢大家的反馈，这个是nRF-Toolbox停更OC版本之前的项目，3.0版本，最新的nRF是4.4.4，我给挪过来了。其中用于DFU升级的库我前几天更新了下，是较新的版本，但仍可能会存在隐藏问题。
+这个项目的目的是为不熟悉swift的开发者提供一个参考，但我也能力有限，另一方面感觉也没有必要，持续跟进对应swift版本的更新。所以遇到开发问题，应该首先考虑版本差异，其次参考官网的做法[IOS-nRF-Toolbox](https://github.com/NordicSemiconductor/IOS-nRF-Toolbox)，谢谢大家。
+
+2018-2-1
+
+
+
+# IOS-nRF-Toolbox
+
+The nRF Toolbox is a container app that stores your Nordic Semiconductor apps for Bluetooth Smart in one location. 
+
+The current version is 3.0. 
+
+New in 3.0 version:
+* The application uses DFU Library, instead of having it's own implementation. See [IOS-DFU-Library](https://github.com/NordicSemiconductor/IOS-DFU-Library).
+
+New in 2.5 version:
+* Refreshed Look & Feel
+* Better user experience in DFU and UART profiles
+* Bug fixes
+
+It contains applications demonstrating Bluetooth Smart profiles: 
+* **Cycling Speed and Cadence**, 
+* **Running Speed and Cadence**, 
+* **Heart Rate Monitor**, 
+* **Blood Pressure Monitor**, 
+* **Health Thermometer Monitor**, 
+* **Glucose Monitor**,
+* **Proximity Monitor**. 
+
+### Device Firmware Update
+
+The **Device Firmware Update (DFU)** profile allows you to update the application, bootloader and/or the Soft Device image over-the-air (OTA). It is compatible with Nordic Semiconductor nRF5x devices that have the S-Series SoftDevice and bootloader enabled. From version 1.5 onward, the nRF Toolbox has allowed to send the required init packet. More information about the init packet may be found here: [init packet handling](https://github.com/NordicSemiconductor/nRF-Master-Control-Panel/tree/master/init%20packet%20handling).
+
+The nRF Toolbox 3.0 is using the DFULibrary framework, available here: [IOS-DFU-Library](https://github.com/NordicSemiconductor/IOS-DFU-Library). The library is required to compile the project. Please, follow the steps in this repository to add it to the project.
+
+The DFU has the following features:
+- Scans for devices that are in DFU mode.
+- Connects to devices in DFU mode and uploads the selected firmware (Softdevice, Bootloader and/or application).
+- Allows HEX or BIN file upload through your phone or tablet.
+- Allows to update a Softdevice and/or bootloader and application from ZIP automatically.
+- Pause, resume, and cancel file uploads.
+- Includes pre-installed examples that consist of the Bluetooth Smart heart rate service and running speed and cadence service.
+
+### Note
+- iPhone 4S or newer is required.
+- iPad 3 or newer is required.
+- Compatible with nRF5x devices with S-Series Softdevice and DFU Bootloader flashed.
+- nRF51 and nRF52 Development kits can be ordered from http://www.nordicsemi.com/eng/Buy-Online.
+- The SDK and SoftDevices are available online at http://developer.nordicsemi.com.
---
+++ nRF Toolbox/DFU/DFUViewController/DFUViewController.m
@@ -0,0 +1,442 @@
+/*
+ * Copyright (c) 2015, Nordic Semiconductor
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "DFUViewController.h"
+#import "ScannerViewController.h"
+
+#import "Constants.h"
+#import "AppFilesViewController.h"
+#import "UserFilesViewController.h"
+#import "Utility.h"
+
+@interface DFUViewController ()
+
+/*!
+ * This property is set when the device has been selected on the Scanner View Controller.
+ */
+@property (strong, nonatomic) CBPeripheral *selectedPeripheral;
+@property (strong, nonatomic) CBCentralManager *centralManager;
+@property (strong, nonatomic) DFUServiceController *controller;
+@property (strong, nonatomic) DFUFirmware *selectedFirmware;
+@property (strong, nonatomic) NSURL *selectedFileUrl;
+@property (strong, nonatomic) NSURL *selectedFileDataUrl;
+
+@property (weak, nonatomic) IBOutlet UILabel *fileName;
+@property (weak, nonatomic) IBOutlet UILabel *fileSize;
+
+@property (weak, nonatomic) IBOutlet UILabel *uploadStatus;
+@property (weak, nonatomic) IBOutlet UIProgressView *progress;
+@property (weak, nonatomic) IBOutlet UILabel *progressLabel;
+@property (weak, nonatomic) IBOutlet UIButton *selectFileButton;
+@property (weak, nonatomic) IBOutlet UIView *uploadPane;
+@property (weak, nonatomic) IBOutlet UIButton *uploadButton;
+@property (weak, nonatomic) IBOutlet UILabel *fileType;
+@property (weak, nonatomic) IBOutlet UILabel *verticalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *deviceName;
+@property (weak, nonatomic) IBOutlet UIButton *connectButton;
+
+- (IBAction)uploadPressed;
+- (IBAction)aboutButtonClicked:(id)sender;
+
+@end
+
+@implementation DFUViewController
+
+@synthesize deviceName;
+@synthesize connectButton;
+@synthesize selectedPeripheral;
+@synthesize selectedFirmware;
+@synthesize fileName;
+@synthesize fileSize;
+@synthesize uploadStatus;
+@synthesize progress;
+@synthesize progressLabel;
+@synthesize selectFileButton;
+@synthesize uploadButton;
+@synthesize uploadPane;
+@synthesize fileType;
+@synthesize selectedFileUrl;
+@synthesize selectedFileDataUrl;
+@synthesize centralManager;
+@synthesize controller;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];    
+    // Rotate the vertical label
+    self.verticalLabel.transform = CGAffineTransformRotate(CGAffineTransformMakeTranslation(-145.0f, 0.0f), (float)(-M_PI / 2));
+   
+}
+
+-(void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:YES];
+    //if DFU peripheral is connected and user press Back button then disconnect it
+    if ([self isMovingFromParentViewController] && controller != nil)
+    {
+        if (![controller abort]) {
+            [self logWith:LogLevelApplication message:@"Aborting DFU process failed"];
+        }
+    }
+}
+
+-(void)uploadPressed
+{
+    if (controller)
+    {
+        // Pause the upload process. Pausing is possible only during upload, so if the device was still connecting or sending some metadata it will continue to do so,
+        // but it will pause just before seding the data.
+        [controller pause];
+        
+        UIAlertController * alert = [UIAlertController alertControllerWithTitle:@"Abort?" message:@"Do you want to abort?" preferredStyle:UIAlertControllerStyleActionSheet];
+        UIAlertAction* abort = [UIAlertAction
+                                actionWithTitle:@"Abort"
+                                style:UIAlertActionStyleDestructive
+                                handler:^(UIAlertAction * action)
+                                {
+                                    // Abort upload process
+                                    [controller abort];
+                                    [alert dismissViewControllerAnimated:YES completion:nil];
+                                    
+                                }];
+        UIAlertAction* cancel = [UIAlertAction
+                                 actionWithTitle:@"Cancel"
+                                 style:UIAlertActionStyleDefault
+                                 handler:^(UIAlertAction * action)
+                                 {
+                                     // Resume upload
+                                     [controller resume];
+                                     [alert dismissViewControllerAnimated:YES completion:nil];
+                                 }];
+        
+        [alert addAction:abort];
+        [alert addAction:cancel];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+    else
+    {
+        [self performDFU];
+    }
+}
+
+- (IBAction)aboutButtonClicked:(id)sender {
+    [self showAbout:[Utility getDFUHelpText]];
+}
+
+-(void)performDFU
+{
+    [self disableOtherButtons];
+    progress.hidden = NO;
+    progressLabel.hidden = NO;
+    uploadStatus.hidden = NO;
+    uploadButton.enabled = NO;
+    
+    [self registerObservers];
+    
+    // To start the DFU operation the DFUServiceInitiator must be used
+    
+    //DFUServiceInitiator *initiator = [[DFUServiceInitiator alloc] initWithCentralManager: centralManager target:selectedPeripheral]; //deprecated
+    
+    dispatch_queue_t queue = dispatch_queue_create("com.nRF.customQueue", NULL);
+    DFUServiceInitiator *initiator = [[DFUServiceInitiator alloc] initWithQueue:queue]; //recommend
+    /** 旧方法 */
+    //[initiator withFirmwareFile:selectedFirmware];
+    /** 新方法 */
+    initiator = [initiator withFirmware:selectedFirmware];
+    initiator.forceDfu = [[[NSUserDefaults standardUserDefaults] valueForKey:@"dfu_force_dfu"] boolValue];
+    initiator.packetReceiptNotificationParameter = [[[NSUserDefaults standardUserDefaults] valueForKey:@"dfu_number_of_packets"] intValue];
+    initiator.logger = self;
+    initiator.delegate = self;
+    initiator.progressDelegate = self;
+    initiator.enableUnsafeExperimentalButtonlessServiceInSecureDfu = YES;
+    // initiator.peripheralSelector = ... // the default selector is used
+    
+    //controller = [initiator start];  deprecated
+    controller = [initiator startWithTarget:selectedPeripheral];  //recommend
+    [uploadButton setTitle:@"Cancel" forState:UIControlStateNormal];
+    uploadButton.enabled = YES;
+}
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"scan"])
+    {
+        // Set this contoller as scanner delegate
+        UINavigationController *nc = segue.destinationViewController;
+        ScannerViewController *scannerViewController = (ScannerViewController *)nc.childViewControllerForStatusBarHidden;
+        //controller.filterUUID = dfuServiceUUID; - the DFU service should not be advertised. We have to scan for any device hoping it supports DFU.
+        scannerViewController.delegate = self;
+    }
+    else if ([segue.identifier isEqualToString:@"FileSegue"])
+    {
+        UINavigationController* nc = segue.destinationViewController;
+        UITabBarController *barController = (UITabBarController*) nc.childViewControllerForStatusBarHidden;
+        AppFilesViewController *appFilesVC = [barController.viewControllers firstObject];
+        appFilesVC.fileDelegate = self;
+        UserFilesViewController* userFilesVC = [barController.viewControllers lastObject];
+        userFilesVC.fileDelegate = self;
+        
+        if (selectedFileUrl)
+        {
+            NSString *path = [selectedFileUrl path];
+            appFilesVC.selectedPath = path;
+            userFilesVC.selectedPath = path;
+        }
+    }
+}
+
+- (void) clearUI
+{
+    controller = nil;
+    selectedPeripheral = nil;
+    
+    deviceName.text = @"DEFAULT DFU";
+    uploadStatus.text = nil;
+    uploadStatus.hidden = YES;
+    progress.progress = 0.0f;
+    progress.hidden = YES;
+    progressLabel.text = nil;
+    progressLabel.hidden = YES;
+    
+    [uploadButton setTitle:@"Upload" forState:UIControlStateNormal];
+    [self enableOrDisableUploadButton];
+    [self enableOtherButtons];
+    
+    [self unregisterObservers];
+}
+
+-(void)enableOrDisableUploadButton
+{
+    uploadButton.enabled = selectedFirmware && selectedPeripheral;
+}
+
+-(void)disableOtherButtons
+{
+    selectFileButton.enabled = NO;
+    connectButton.enabled = NO;
+}
+
+-(void)enableOtherButtons
+{
+    selectFileButton.enabled = YES;
+    connectButton.enabled = YES;
+}
+
+#pragma mark - Supoprt for background mode
+
+-(void)registerObservers
+{
+    if ([UIApplication instancesRespondToSelector:@selector(registerUserNotificationSettings:)])
+    {
+        [[UIApplication sharedApplication] registerUserNotificationSettings:
+         [UIUserNotificationSettings
+          settingsForTypes: UIUserNotificationTypeAlert|UIUserNotificationTypeSound
+          categories:nil]];
+    }
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidEnterBackground:) name:UIApplicationDidEnterBackgroundNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidEnterForeground:) name:UIApplicationDidBecomeActiveNotification object:nil];
+}
+
+-(void)unregisterObservers
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidBecomeActiveNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidEnterBackgroundNotification object:nil];
+}
+
+-(void)appDidEnterBackground:(NSNotification *)_notification
+{
+    // Controller is set when the DFU is in progress
+    if (controller)
+    {
+        [Utility showBackgroundNotification:@"Uploading firmware..."];
+    }
+}
+
+-(void)appDidEnterForeground:(NSNotification *)_notification
+{
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+}
+
+#pragma mark - Device selection delegate methods
+
+-(void)centralManager:(CBCentralManager *)manager didPeripheralSelected:(CBPeripheral *)peripheral
+{
+    selectedPeripheral = peripheral;
+    centralManager = manager;
+    deviceName.text = peripheral.name;
+    [self enableOrDisableUploadButton];
+}
+
+#pragma mark - DFU Service delegate methods
+
+-(void)logWith:(enum LogLevel)level message:(NSString *)message
+{
+    NSLog(@"%ld: %@", (long) level, message);
+}
+
+-(void)dfuStateDidChangeTo:(enum DFUState)state
+{
+    switch (state) {
+        case DFUStateConnecting:
+            uploadStatus.text = @"Connecting...";
+            break;
+        case DFUStateStarting:
+            uploadStatus.text = @"Starting DFU...";
+            break;
+        case DFUStateEnablingDfuMode:
+            uploadStatus.text = @"Enabling DFU Bootloader...";
+            break;
+        case DFUStateUploading:
+            uploadStatus.text = @"Uploading...";
+            break;
+        case DFUStateValidating:
+            uploadStatus.text = @"Validating...";
+            progressLabel.text = nil;
+            break;
+        case DFUStateDisconnecting:
+            uploadStatus.text = @"Disconnecting...";
+            break;
+        case DFUStateCompleted:
+            [Utility showAlert:@"Upload complete"];
+            if ([Utility isApplicationStateInactiveORBackground])
+            {
+                [Utility showBackgroundNotification:@"Upload complete."];
+            }
+            [self clearUI];
+            break;
+        case DFUStateAborted:
+            [Utility showAlert:@"Upload aborted"];
+            [self clearUI];
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)dfuProgressDidChangeFor:(NSInteger)part outOf:(NSInteger)totalParts to:(NSInteger)progress currentSpeedBytesPerSecond:(double)currentSpeedBytesPerSecond avgSpeedBytesPerSecond:(double)avgSpeedBytesPerSecond{
+    
+    NSLog(@"part:%ld, totalParts:%ld, progress:%ld, currentSpeedBytesPerSecond:%f, avgSpeedBytesPerSecond:%f", part, totalParts, progress, currentSpeedBytesPerSecond, avgSpeedBytesPerSecond);
+    //打印更新进度
+    self.progress.progress = progress / 100.0;
+    self.progressLabel.text = [NSString stringWithFormat:@"%ld%% (%ld/%ld)",progress,part,totalParts];
+}
+- (void)dfuError:(enum DFUError)error didOccurWithMessage:(NSString * _Nonnull)message{
+    
+    NSLog(@"Error %ld: %@", (long) error, message);
+    
+    [Utility showAlert:message];
+    if ([Utility isApplicationStateInactiveORBackground])
+    {
+        [Utility showBackgroundNotification:message];
+    }
+    [self clearUI];
+}
+
+#pragma mark - File selection delegate methods
+
+-(void)onFileSelected:(NSURL *)url
+{
+    selectedFileUrl = url;
+    selectedFileDataUrl = [NSURL fileURLWithPath:@"file:///var/containers/Bundle/Application/E4F79CED-6495-49B8-95DA-54B0F4E68CC0/nRF%20Toolbox.app/firmwares/X9ET_4.3.0.dat"];
+    selectedFirmware = nil;
+    fileName.text = nil;
+    fileSize.text = nil;
+    fileType.text = nil;
+    
+    NSString *fileNameComponent = url.lastPathComponent;
+    NSString *extension = [[fileNameComponent pathExtension] lowercaseString];
+    
+    if ([extension isEqualToString:@"zip"])
+    {
+        selectedFirmware = [[DFUFirmware alloc] initWithUrlToZipFile:url];
+        
+        if (selectedFirmware && selectedFirmware.fileName)
+        {
+            fileName.text = selectedFirmware.fileName;
+            NSData *content = [[NSData alloc] initWithContentsOfURL:url];
+            fileSize.text = [NSString stringWithFormat:@"%lu bytes", (long) content.length];
+            fileType.text = @"Distribution Packet (ZIP)";
+        }
+        else
+        {
+            selectedFirmware = nil;
+            selectedFileUrl = nil;
+            [Utility showAlert:@"Selected file is not supported."];
+        }
+        [self enableOrDisableUploadButton];
+    }
+    else
+    {
+        // Show a view to select the file type
+        UIStoryboard *mainStoryboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        UINavigationController *nc = (UINavigationController *) [mainStoryboard instantiateViewControllerWithIdentifier:@"SelectFileType"];
+        FileTypeViewController *fileTypeVC = (FileTypeViewController *) nc.childViewControllerForStatusBarHidden;
+        fileTypeVC.delegate = self;
+        [self presentViewController:nc animated:YES completion:nil];
+    }
+}
+
+-(void)onFileTypeSelected:(DFUFirmwareType)type
+{
+    
+    selectedFirmware = [[DFUFirmware alloc] initWithUrlToBinOrHexFile:selectedFileUrl urlToDatFile:nil type:type];
+//    selectedFirmware = [[DFUFirmware alloc] initWithUrlToBinOrHexFile:selectedFileUrl urlToDatFile:selectedFileDataUrl type:type];
+    
+    if (selectedFirmware && selectedFirmware.fileName)
+    {
+        fileName.text = selectedFirmware.fileName;
+        NSData *content = [[NSData alloc] initWithContentsOfURL:selectedFileUrl];
+        fileSize.text = [NSString stringWithFormat:@"%lu bytes", (long) content.length];
+        
+        switch (type) {
+            case DFUFirmwareTypeSoftdevice:
+                fileType.text = @"Softdevice";
+                break;
+            case DFUFirmwareTypeBootloader:
+                fileType.text = @"Bootloader";
+                break;
+            case DFUFirmwareTypeApplication:
+                fileType.text = @"Application";
+                break;
+            case DFUFirmwareTypeSoftdeviceBootloader:
+                fileType.text = @"SD + BL";
+                break;
+            case DFUFirmwareTypeSoftdeviceBootloaderApplication:
+                fileType.text = @"APP + SD + BL";
+                break;
+        }
+    }else{
+        
+        selectedFirmware = nil;
+        selectedFileUrl = nil;
+        [Utility showAlert:@"Selected file is not supported."];
+    }
+    [self enableOrDisableUploadButton];
+}
+
+-(void)onFileTypeNotSelected
+{
+    selectedFileUrl = nil;
+    [self enableOrDisableUploadButton];
+}
+
+@end
